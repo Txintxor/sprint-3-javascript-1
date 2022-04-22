0bb5ac60b8765559d2eaecd591e2c5397cb90f92
AlexQ <78956966+Txintxor@users.noreply.github.com>
2022-04-22 13:25:45 +0200

    Fix al cambiar de correcte a incorrecte als inputs de la form

--- nivell-1/js/checkout.js
+++ nivell-1/js/checkout.js
@@ -14,7 +14,6 @@ let regExMail = /[.\w]+@([\w-]+\.)+([\w-]+){3,}/;
 let regExLetter = /^[a-z]{3,}$/i;
 let regExPass = /(\w*\d+\w*){3,}/gi;
 
-
 //Functions to check input fields
 let mailVal = (val) => regExMail.test(val);
 let letterVal = (val) => regExLetter.test(val);
@@ -23,69 +22,108 @@ let passVal = (val) => regExPass.test(val);
 // Exercise 6
 function validate() {
   // Validate fields entered by the user: name, phone, password, and email
-  
-  //Object used to store client data 
+
+  //Object used to store client data
   let dataObject = {};
 
   //Function used to alert on input errors
   function inputError(id, errorId) {
     let element = document.getElementById(id);
     let errorElement = document.getElementById(errorId);
-    let classErrorArray = ['border', 'border-danger', 'text-danger']
-    errorElement.className = 'rounded';
-    for (let i = 0; i < classErrorArray.length; i++) {    
-       element.classList.add(classErrorArray[i]);   
-       errorElement.classList.add(classErrorArray[i]);         
+    let classErrorArray = ["border", "border-danger", "text-danger"];
+
+    errorElement.className = "rounded";
+
+    for (let i = 0; i < classErrorArray.length; i++) {
+      element.classList.add(classErrorArray[i]);
+      errorElement.classList.add(classErrorArray[i]);
+    }
+  }
+
+  //Function used to refresh correct inputs
+  function correctInput(id, correctId) {
+    let element = document.getElementById(id);
+    let correctElement = document.getElementById(correctId);
+    let classCorrectArray = ["form-control",  "ml-3"];
+
+    correctElement.className = "invalid-feedback";
+    element.className = "";
+    for (let i = 0; i < classCorrectArray.length; i++) {
+      element.classList.add(classCorrectArray[i]);
     }
   }
 
   //Input check and validation
   function inputCheck(id) {
-    if (id.value == '') {
+    if (id.value == "") {
       switch (id) {
         case fName:
-          inputError('fName', 'errorName');
+          inputError("fName", "errorName");
           break;
         case fEmail:
-          inputError('fEmail', 'errorEmail');
+          inputError("fEmail", "errorEmail");
           break;
         case fAddress:
-          inputError('fAddress', 'errorAddress');
+          inputError("fAddress", "errorAddress");
           break;
         case fLastN:
-          inputError('fLastN', 'errorLastN');
+          inputError("fLastN", "errorLastN");
           break;
         case fPassword:
-          inputError('fPassword', 'errorPassword');
+          inputError("fPassword", "errorPassword");
           break;
         case fPhone:
-          inputError('fPhone', 'errorPhone');
+          inputError("fPhone", "errorPhone");
           break;
       }
     } else {
       switch (id) {
         case fName:
           dataObject.Name = id.value;
-          if (!letterVal(dataObject.Name)) {inputError('fName', 'errorName')}
+          if (!letterVal(dataObject.Name)) {
+            inputError("fName", "errorName");
+          } else {
+            correctInput("fName", "errorName");
+          }
           break;
         case fEmail:
           dataObject.Email = id.value;
-          if (!mailVal(dataObject.Email)) {inputError('fEmail', 'errorEmail')}
+          if (!mailVal(dataObject.Email)) {
+            inputError("fEmail", "errorEmail");
+          } else {
+            correctInput("fEmail", "errorEmail");
+          }
           break;
         case fAddress:
           dataObject.Address = id.value;
-          if ((dataObject.Address).lenght < 3 ) {inputError('fAddress', 'errorAddress')}
+          if (dataObject.Address.lenght < 3) {
+            inputError("fAddress", "errorAddress");
+          } else {
+            correctInput("fAddress", "errorAddress");
+          }
           break;
         case fLastN:
           dataObject.LastName = id.value;
-          if (!letterVal(dataObject.LastName)) {inputError('fLastN', 'errorLastN')}
+          if (!letterVal(dataObject.LastName)) {
+            inputError("fLastN", "errorLastN");
+          } else {
+            correctInput("fLastN", "errorLastN");
+          }
           break;
         case fPassword:
           dataObject.Password = id.value;
-          if (!passVal(dataObject.Password)) {inputError('fPassword', 'errorPassword')}
+          if (!passVal(dataObject.Password)) {
+            inputError("fPassword", "errorPassword");
+          } else {
+            correctInput("fPassword", "errorPassword");
+          }
         case fPhone:
           dataObject.Phone = id.value;
-          if (letterVal(dataObject.Phone)) {inputError('fPhone', 'errorPhone')}
+          if (letterVal(dataObject.Phone)) {
+            inputError("fPhone", "errorPhone");
+          } else {
+            correctInput("fPhone", "errorPhone");
+          }
           break;
       }
     }
