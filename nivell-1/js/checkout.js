f1bb1d91353fa595ab95ba3b64bfbad0e711c3a0
AlexQ <78956966+Txintxor@users.noreply.github.com>
2022-04-29 13:10:59 +0200

    Exercici 6 Fixed

--- nivell-1/js/checkout.js
+++ nivell-1/js/checkout.js
@@ -1,152 +1,190 @@
 //Data input variables
-let valFname;
-let valEmail;
-let valAddress;
-let valSname;
-let valPass;
-let valPhone;
+let valFname, valEmail, valAddress, valSname, valPass, valPhone;
 
+//Array to store input id´s
+let inputArray = [
+  "fName",
+  "fEmail",
+  "fAddress",
+  "fLastN",
+  "fPassword",
+  "fPhone",
+];
 
+//Input flags used to validate form
+let fName = (fEmail = fAddress = fLastN = fPassword = fPhone = false);
 
-//Array to store input variables
-let fName;
-let fEmail;
-let fAddress;
-let fLastN;
-let fPassword;
-let fPhone;
-let inputArray = [fName, fEmail, fAddress, fLastN, fPassword, fPhone];
+let flagArray = [fName, fEmail, fAddress, fLastN, fPassword, fPhone];
+
+//Variable which stores the data output
+let dataObject = {};
 
 //Regular Expressions checkers for input fields
 let regExMail = /[.\w]+@([\w-]+\.)+([\w-]+){3,}/;
 let regExLetter = /^[a-z]{3,}$/i;
 let regExPass = /(\w*\d+[a-z]+\w*)/gi;
 
-//Functions to check input fields
+//Regex Validation functions
 let mailVal = (val) => regExMail.test(val);
 let letterVal = (val) => regExLetter.test(val);
 let passVal = (val) => regExPass.test(val);
 
-// Exercise 6
-function validate() {
-  // Validate fields entered by the user: name, phone, password, and email
+//Function used to refresh correct inputs and flag them as correct
+function correctInput(id, correctId) {
+  let element = document.getElementById(id);
+  let correctElement = document.getElementById(correctId);
+  let classCorrectArray = ["form-control", "ml-3"];
 
-  //Object used to store client data
-  let dataObject = {};
+  correctElement.className = "invalid-feedback";
+  element.className = "";
+  for (let i = 0; i < classCorrectArray.length; i++) {
+    element.classList.add(classCorrectArray[i]);
+  }
 
-  //Function used to alert on input errors
-  function inputError(id, errorId) {
-    let element = document.getElementById(id);
-    let errorElement = document.getElementById(errorId);
-
-
-    //As validations were disabled from bootstrap 5 
-
-    //As the validations were disabled from bootstrap 5 
-
-    //https://getbootstrap.com/docs/5.0/forms/validation/
-    //I decided to change de classes by hand
-    //Don´t know if it´s the proper approach but it seems to function just fine
-    let classErrorArray = ["border", "border-danger", "text-danger"];
-
-    errorElement.className = "rounded";
-
-    for (let i = 0; i < classErrorArray.length; i++) {
-      element.classList.add(classErrorArray[i]);
-      errorElement.classList.add(classErrorArray[i]);
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] == id) {
+      flagArray[i] = true;
     }
   }
 
-  //Function used to refresh correct inputs
-  function correctInput(id, correctId) {
-    let element = document.getElementById(id);
-    let correctElement = document.getElementById(correctId);
-    let classCorrectArray = ["form-control",  "ml-3"];
+  // switch (id) {
+  //   case "fName":
+  //     fName = true;
+  //     break;
+  //   case "fEmail":
+  //     fEmail = true;
+  //     break;
+  //   case "fAddress":
+  //     fAddress = true;
+  //     break;
+  //   case "fLastN":
+  //     fLastN = true;
+  //     break;
+  //   case "fPassword":
+  //     fPassword = true;
+  //     break;
+  //   case "fPhone":
+  //     fPhone = true;
+  //     break;
+  // }
+}
+//Function used to alert on input errors
+function inputError(id, errorId) {
+  let element = document.getElementById(id);
+  let errorElement = document.getElementById(errorId);
+  let classErrorArray = ["border", "border-danger", "text-danger"];
 
-    correctElement.className = "invalid-feedback";
-    element.className = "";
-    for (let i = 0; i < classCorrectArray.length; i++) {
-      element.classList.add(classCorrectArray[i]);
-    }
-  }
+  errorElement.className = "rounded";
 
-  //Input check and validation
-  function inputCheck(id) {
-    if (id.value == "") {
-      switch (id) {
-        case fName:
-          inputError("fName", "errorName");
-          break;
-        case fEmail:
-          inputError("fEmail", "errorEmail");
-          break;
-        case fAddress:
-          inputError("fAddress", "errorAddress");
-          break;
-        case fLastN:
-          inputError("fLastN", "errorLastN");
-          break;
-        case fPassword:
-          inputError("fPassword", "errorPassword");
-          break;
-        case fPhone:
-          inputError("fPhone", "errorPhone");
-          break;
-      }
-    } else {
-      switch (id) {
-        case fName:
-          dataObject.Name = id.value;
-          if (!letterVal(dataObject.Name)) {
-            inputError("fName", "errorName");
-          } else {
-            correctInput("fName", "errorName");
-          }
-          break;
-        case fEmail:
-          dataObject.Email = id.value;
-          if (!mailVal(dataObject.Email)) {
-            inputError("fEmail", "errorEmail");
-          } else {
-            correctInput("fEmail", "errorEmail");
-          }
-          break;
-        case fAddress:
-          dataObject.Address = id.value;
-          if (dataObject.Address.lenght < 3) {
-            inputError("fAddress", "errorAddress");
-          } else {
-            correctInput("fAddress", "errorAddress");
-          }
-          break;
-        case fLastN:
-          dataObject.LastName = id.value;
-          if (!letterVal(dataObject.LastName)) {
-            inputError("fLastN", "errorLastN");
-          } else {
-            correctInput("fLastN", "errorLastN");
-          }
-          break;
-        case fPassword:
-          dataObject.Password = id.value;
-          if (!passVal(dataObject.Password)) {
-            inputError("fPassword", "errorPassword");
-          } else {
-            correctInput("fPassword", "errorPassword");
-          }
-        case fPhone:
-          dataObject.Phone = id.value;
-          if (dataObject.Phone.length < 9) {
-            inputError("fPhone", "errorPhone");
-          } else {
-            correctInput("fPhone", "errorPhone");
-          }
-          break;
-      }
-    }
+  for (let i = 0; i < classErrorArray.length; i++) {
+    element.classList.add(classErrorArray[i]);
+    errorElement.classList.add(classErrorArray[i]);
   }
   for (let i = 0; i < inputArray.length; i++) {
-    inputCheck(inputArray[i]);
+    if (inputArray[i] == id) {
+      flagArray[i] = false;
+    }
   }
-  return dataObject;
+
+  // switch (id) {
+  //   case "fName":
+  //     fName = false;
+  //     break;
+  //   case "fEmail":
+  //     fEmail = false;
+  //     break;
+  //   case "fAddress":
+  //     fAddress = false;
+  //     break;
+  //   case "fLastN":
+  //     fLastN = false;
+  //     break;
+  //   case "fPassword":
+  //     fPassword = false;
+  //     break;
+  //   case "fPhone":
+  //     fPhone = false;
+  //     break;
+  // }
 }
+
+function inputCheck(id, value) {
+  switch (id) {
+    case "fName":
+      dataObject.name = value;
+      if (!letterVal(dataObject.name)) {
+        inputError("fName", "errorName");
+      } else {
+        correctInput("fName", "errorName");
+      }
+      break;
+    case "fEmail":
+      dataObject.email = value;
+      if (!mailVal(dataObject.email)) {
+        inputError("fEmail", "errorEmail");
+      } else {
+        correctInput("fEmail", "errorEmail");
+      }
+      break;
+    case "fAddress":
+      dataObject.address = value;
+      if (dataObject.address.length < 3) {
+        inputError("fAddress", "errorAddress");
+      } else {
+        correctInput("fAddress", "errorAddress");
+      }
+      break;
+    case "fLastN":
+      dataObject.lastName = value;
+      if (!letterVal(dataObject.lastName)) {
+        inputError("fLastN", "errorLastN");
+      } else {
+        correctInput("fLastN", "errorLastN");
+      }
+      break;
+    case "fPassword":
+      dataObject.password = value;
+      if (!passVal(dataObject.password)) {
+        inputError("fPassword", "errorPassword");
+      } else {
+        correctInput("fPassword", "errorPassword");
+      }
+    case "fPhone":
+      dataObject.phone = value;
+      if (dataObject.phone.length < 9) {
+        inputError("fPhone", "errorPhone");
+      } else {
+        correctInput("fPhone", "errorPhone");
+      }
+      break;
+  }
+}
+
+function validate(event) {
+  // Check if fields are correctly filled
+  for (let i = 0; i < flagArray.length; i++) {
+    if (!flagArray[i]) {
+      event.preventDefault();
+      alert("Check and correct marked fields");
+      return false;
+    } else {
+      alert("Good job");
+      return dataObject;
+    }
+  }
+}
+
+//Input event listener
+
+// let form = document.getElementById('form');
+// document.addEventListener("focusout", (event) => inputCheck(event.target.id) );
+
+// function hunter(event) {
+//   let target = event.target.id;
+//   inputCheck(target);
+//   }
+
+let form = document.getElementById("form");
+form.addEventListener("focusout", (e) =>
+  inputCheck(e.target.id, e.target.value)
+);
